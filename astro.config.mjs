5eb26ee3ac021767a2886bf4911dc7029a04c07b
ttntm <ttntm@pm.me>
2024-03-04 14:24:04 +0100

    fix: baseURL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import vue from '@astrojs/vue'
 
-const baseURL = process.env.NODE_ENV === 'production' ? 'https://prod.site/' : 'http://localhost:4321/'
+const baseURL = process.env.NODE_ENV === 'production'
+  ? 'https://ttntm.github.io/astro-potfolio-starter/'
+  : 'http://localhost:4321/'
 
 // https://astro.build/config
 // sitemap: https://docs.astro.build/en/guides/integrations-guide/sitemap/#configuration
